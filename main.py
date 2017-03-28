9ce4796317b916b11b39631757cc4f2fc44cb422
Thin, Thang (tt5822) <tt5822@us.att.com>
2017-03-28 15:51:46 -0500

    added comment to init method in movie class and main file condition check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import fresh_tomatoes
 from list_of_movies import LIST_OF_MOVIES
 
+# only create html and run in browser if this was the main file being executed
 if __name__ == "__main__":
     fresh_tomatoes.open_movies_page(LIST_OF_MOVIES)
--- movie.py
+++ movie.py
@@ -1,6 +1,12 @@
 class Movie:
     """A Movie Class for Movie Template"""
     def __init__(self, title, trailer_url, image_path):
+        """Initialize movie instance
+        Args:
+            title (str): title of Movie
+            trailer_url (str): url link for movie's trailer_url
+            image_path (str): url link for movie's cover image
+        """
         self.title = title
         self.trailer_youtube_url = trailer_url
         self.poster_image_url = image_path
